3390247a3cfd0e55d45b4fc56681282d00bfb581
apple <1>
2018-09-15 13:59:25 +0300

    Action creator code optimization

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,26 +1,18 @@
 // Action creators can run async code
 import * as actionTypes from './actionTypes'
 
-export const loggedIn = email => {
-  return dispatch => {
-    dispatch({
-      type: actionTypes.LOGGED_IN,
-      payload: {
-        isAuthed: true,
-        email
-      }
-    })
+export const loggedIn = email => ({
+  type: actionTypes.LOGGED_IN,
+  payload: {
+    isAuthed: true,
+    email
   }
-}
+})
 
-export const loggedOut = () => {
-  return dispatch => {
-    dispatch({
-      type: actionTypes.LOGGED_IN,
-      payload: {
-        isAuthed: false,
-        email: ''
-      }
-    })
+export const loggedOut = () => ({
+  type: actionTypes.LOGGED_IN,
+  payload: {
+    isAuthed: false,
+    email: ''
   }
-}
+})
